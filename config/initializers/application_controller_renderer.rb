39dc9dac609a51ff6176c81770951d3ecbc3cbc0
飯田恭子 <iidakyouko@mba.local>
2020-04-21 02:16:44 +0900

    [Start meshiterro]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '074982d165507608ccf6964ab613d8557a8abfaf13009faf68abd53916436322e3aa2e976c3bd84b713e6cc93e6c94a7ccf2030954457491cd36b6341c5351ad'
